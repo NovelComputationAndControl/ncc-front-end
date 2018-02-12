edd2778663e020d59f0479424bbc6b7014edb601
Denis Nuțiu <denis.nutiu@gmail.com>
2018-02-12 21:38:21 +0200

    Tweaking author cards

--- src/Components/AuthorCard/AuthorCard.js
+++ src/Components/AuthorCard/AuthorCard.js
@@ -1,26 +1,51 @@
 import React from 'react'
 import "./AuthorCard.css"
 
+import Icon from 'react-icons-kit';
+import {twitter} from 'react-icons-kit/fa/twitter';
+import {globe} from 'react-icons-kit/fa/globe';
+
 import {
     Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle
 } from 'reactstrap';
 
-import UnknownImage from './unknown.png'
-
 class AuthorCard extends React.Component {
-
     render() {
+        let authorImage = null;
+        let twitterIcon = null;
+        let websiteIcon = null;
+
+        if (this.props.image) {
+            authorImage = <CardImg className="hidden-sm-down" top
+                                   src={this.props.image}
+                                   alt={this.props.name + "'s photo"}/>
+        }
+
+        if (this.props.twitter) {
+            twitterIcon = <a href={"https://twitter.com/" + this.props.twitter} target="_blank">
+                <Icon size={32} icon={twitter}/>
+            </a>
+        }
+
+        if (this.props.website) {
+            websiteIcon = <a href={this.props.website} target="_blank">
+                <Icon size={32} icon={globe}/>
+            </a>
+        }
+
         return (
             <div>
                 <Card>
-                    <CardImg className="hidden-sm-down" top
-                             src={ this.props.image || UnknownImage }
-                             alt={ this.props.name + "'s photo" }/>
+                    {authorImage}
                     <CardBody>
                         <CardTitle className="text-center">{this.props.name}</CardTitle>
                         <CardSubtitle className="text-center font-weight-bold">{this.props.title}</CardSubtitle>
                         <CardText className="text-justify text-center">{this.props.description}</CardText>
+                        <div className="text-center">
+                            {websiteIcon}
+                            {twitterIcon}
+                        </div>
                     </CardBody>
                 </Card>
             </div>
--- src/Pages/Index/Index.js
+++ src/Pages/Index/Index.js
@@ -3,9 +3,6 @@ import JournalNavbar from '../../Components/JournalNavbar/JournalNavbar';
 import JournalCarousel from "../../Components/JournalCarousel/JournalCarousel";
 import './Index.css';
 import AuthorCard from "../../Components/AuthorCard/AuthorCard";
-import AuthorCircle from "../../Components/AuthorCircle/AutorCircle";
-
-import MihaiImage from "./mihai.png";
 import JournalFooter from "../../Components/JournalFooter/JournalFooter";
 
 class IndexPage extends React.Component {
@@ -23,43 +20,32 @@ class IndexPage extends React.Component {
                         <div className="container">
                             <div className="p-2"></div>
                             <div>
-                                <h2 className="p-2 text-center">Our Staff</h2>
-                                <hr/>
+                                <div className="text-center">
+                                    <a href="/about"><h2>Editorial Board</h2></a>
+                                </div>
                                 <div className="d-flex flex-wrap justify-content-around">
                                     <div className="p-2 child">
-                                        <AuthorCard image={MihaiImage} name="Mihai Udrescu" title="Editor-in-Chief"
-                                                    description="Mihai Udrescu is a proffesor at Universitatea Politehnica Timisoara."/>
+                                        <AuthorCard name="Mihai Udrescu" title="Editor-in-Chief"
+                                                    description="Universitatea Politehnica Timisoara."
+                                                    website="https://peerj.com/MUdrescu/" twitter="MihaiUdrescu"
+                                        />
                                     </div>
                                     <div className="p-2 child">
                                         <AuthorCard name="Radu Marculescu" title="Associate Editor-in-Chief"
-                                                    description="Mihai Udrescu is a proffesor at Universitatea Politehnica Timisoara."/>
+                                                    description="Universitatea Politehnica Timisoara."/>
                                     </div>
                                     <div className="p-2 child">
                                         <AuthorCard name="Paul Bogdan" title="Associate Editor-in-Chief"
-                                                    description="Mihai Udrescu is a proffesor at Universitatea Politehnica Timisoara."/>
+                                                    description="Universitatea Politehnica Timisoara."/>
                                     </div>
                                     <div className="p-2 child">
                                         <AuthorCard name="Lucian Prodan" title="Associate Editor-in-Chief"
-                                                    description="Mihai Udrescu is a proffesor at Universitatea Politehnica Timisoara."/>
+                                                    description="Universitatea Politehnica Timisoara."/>
                                     </div>
                                 </div>
                             </div>
-                            <div className="p-2"></div>
-                            <h2 className="text-center py-2">Editors</h2>
-                            <hr />
-                            <div className="d-flex flex-wrap justify-content-around">
-                                <AuthorCircle name="Hitoshi Oi" university="University of Aizu, Japan"/>
-                                <AuthorCircle name="Jean Luca Tempesi" university="University of York, UK"/>
-                                <AuthorCircle name="Ioan Dumitrache" university="Universitatea Politehnica Bucuresti"/>
-                                <AuthorCircle name="Liviu Miclea" university="Universitatea Tehnica din Cluj Napoca"/>
-                                <AuthorCircle name="Lucian Vintan" university='Universitatea "Lucian Blaga" din Sibiu'/>
-                                <AuthorCircle name="Radu-Emil Precup" university="Universitatea Politehnica Timisoara"/>
-                            </div>
                         </div>
                     </section>
-                    {/*<section>*/}
-                        {/*<h1>About journal.</h1>*/}
-                    {/*</section>*/}
                 </main>
                 <JournalFooter/>
             </div>
