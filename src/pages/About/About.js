abd5034df4eb2eabd9fa33efb582c2f5e017906d
Denis Nuțiu <denis.nutiu@gmail.com>
2018-02-20 23:11:09 +0200

    Adding the editorial board in the about page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import AuthorCard from '../../components/AuthorCard/AuthorCard'
 
 class AboutPage extends React.Component {
 
@@ -18,15 +19,66 @@ class AboutPage extends React.Component {
                         by
                         the Publications Board, which includes operations committees for each publishing line.</p>
                     <h2>Editorial Board</h2>
-                    <p>Here I will add all authors.</p>
-                    <p>Here I will add all authors.</p>
-                    <p>Here I will add all authors.</p>
-                    <p>Here I will add all authors.</p>
+                    <div className="d-flex flex-wrap justify-content-around">
+                        <div className="p-2 child">
+                            <AuthorCard name="Mihai Udrescu" title="Editor-in-Chief"
+                                        description="Universitatea Politehnica Timisoara."
+                                        website="https://peerj.com/MUdrescu/" twitter="MihaiUdrescu"
+                            />
+                        </div>
+                        <div className="p-2 child">
+                            <AuthorCard name="Radu Marculescu" title="Associate Editor-in-Chief"
+                                        description="Universitatea Politehnica Timisoara."
+                                        twitter="rmarculescu" website="https://peerj.com/rmarculescu/"
+                            />
+                        </div>
+                        <div className="p-2 child">
+                            <AuthorCard name="Paul Bogdan" title="Associate Editor-in-Chief"
+                                        description="Universitatea Politehnica Timisoara."
+                                        website="https://viterbi.usc.edu/directory/faculty/Bogdan/Paul"/>
+                        </div>
+                        <div className="p-2 child">
+                            <AuthorCard name="Lucian Prodan" title="Associate Editor-in-Chief"
+                                        description="Universitatea Politehnica Timisoara."
+                                        website="http://www.acsa.upt.ro/about_us/lucian_prodan.htm"
+                            />
+                        </div>
+
+                        <div className="p-2 child">
+                            <AuthorCard name="Hitoshi Oi" title="Editor"
+                                        description="University of Aizu Japan."
+                            />
+                        </div>
+                        <div className="p-2 child">
+                            <AuthorCard name="Jean Luca Tempesi" title="Editor"
+                                        description="University of York UK."
+                            />
+                        </div>
+                        <div className="p-2 child">
+                            <AuthorCard name="Ioan Dumitrache" title="Editor"
+                                        description="Universitatea Politehnica Bucuresti."
+                            />
+                        </div>
+                        <div className="p-2 child">
+                            <AuthorCard name="Liviu Miclea" title="Editor"
+                                        description="Universitatea Tehnica din Cluj Napoca."
+                            />
+                        </div>
+                        <div className="p-2 child">
+                            <AuthorCard name="Lucian Vintan" title="Editor"
+                                        description='Universitatea "Lucian Blaga" din Sibiu.'
+                            />
+                        </div>  
+                        <div className="p-2 child">
+                            <AuthorCard name="Radu-Emil Precup" title="Editor"
+                                        description="Universitatea Politehnica Timisoara."/>
+                        </div>
+                    </div>
                 </article>
 
             </main>
-        )
+    )
     }
-}
+    }
 
-export default AboutPage;
+    export default AboutPage;
